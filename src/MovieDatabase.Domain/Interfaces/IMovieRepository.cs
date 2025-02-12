b885f1990b75be9bbfd081f8397d6bf48165caf6
martrzeciak <mariusz.trzeciak09@gmail.com>
2025-02-12 18:32:03 +0100

    Add IMovieRepository interface

---
+++ src/MovieDatabase.Domain/Interfaces/IMovieRepository.cs
@@ -0,0 +1,12 @@
+﻿using MovieDatabase.Domain.Entities;
+
+namespace MovieDatabase.Domain.Interfaces;
+
+public interface IMovieRepository
+{
+    Task<Movie?> GetByIdAsync(int id);
+    Task<IEnumerable<Movie>> GetAllAsync();
+    Task AddAsync(Movie movie);
+    Task UpdateAsync(Movie movie);
+    Task DeleteAsync(Movie movie);
+}
