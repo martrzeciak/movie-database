2e5bb320148127e1a358354f0dba0228d06cf57c
martrzeciak <mariusz.trzeciak09@gmail.com>
2025-02-12 19:03:43 +0100

    Add DbContext

---
+++ src/MovieDatabase.Infrastructure/Data/AppDbContext.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.EntityFrameworkCore;
+using MovieDatabase.Domain.Entities;
+
+namespace MovieDatabase.Infrastructure.Data;
+
+public class AppDbContext(DbContextOptions<AppDbContext> options) : DbContext(options)
+{
+    public DbSet<Movie> Movies { get; set; }
+}
