47b115070034c7834495fce74082adcac34201c4
martrzeciak <mariusz.trzeciak09@gmail.com>
2025-02-13 22:22:19 +0100

    Add ExceptionMiddleware

---
+++ src/MovieDatabase.API/Middleware/ExceptionMiddleware.cs
@@ -0,0 +1,38 @@
+﻿using System.Net.Mime;
+using System.Text.Json;
+using MovieDatabase.Application.Common;
+
+namespace MovieDatabase.API.Middleware;
+
+public class ExceptionMiddleware(ILogger<ExceptionMiddleware> logger,
+    IHostEnvironment env) : IMiddleware
+{
+    public async Task InvokeAsync(HttpContext context, RequestDelegate next)
+    {
+        try
+        {
+            await next(context);
+        }
+        catch (Exception ex)
+        {
+            await HandleException(context, ex);
+        }
+    }
+
+    private async Task HandleException(HttpContext context, Exception ex)
+    {
+        logger.LogError(ex, ex.Message);
+        context.Response.ContentType = MediaTypeNames.Application.Json;
+        context.Response.StatusCode = StatusCodes.Status500InternalServerError;
+
+        var response = env.IsDevelopment()
+                   ? new AppException(context.Response.StatusCode, ex.Message, ex.StackTrace)
+                   : new AppException(context.Response.StatusCode, ex.Message, "Internal Server Error");
+
+        var options = new JsonSerializerOptions { PropertyNamingPolicy = JsonNamingPolicy.CamelCase };
+
+        var json = JsonSerializer.Serialize(response, options);
+
+        await context.Response.WriteAsync(json);
+    }
+}
