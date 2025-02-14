55d89950598d62d4b8f408ca133ff8c3e795cc9b
martrzeciak <mariusz.trzeciak09@gmail.com>
2025-02-14 18:43:46 +0100

    Update to Angular 19

--- client/movie-database/src/app/app.component.ts
+++ client/movie-database/src/app/app.component.ts
@@ -1,14 +1,9 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { ButtonModule } from 'primeng/button';
-import { FileUploadModule } from 'primeng/fileupload';
-import { MenubarModule } from 'primeng/menubar';
-import { MessageModule } from 'primeng/message';
 
 @Component({
   selector: 'app-root',
-  standalone: true,
-  imports: [RouterOutlet, ButtonModule, FileUploadModule, MenubarModule, MessageModule],
+  imports: [RouterOutlet],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
--- client/movie-database/src/app/app.config.ts
+++ client/movie-database/src/app/app.config.ts
@@ -1,19 +1,8 @@
 import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
-import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
+
 import { routes } from './app.routes';
-import { providePrimeNG } from 'primeng/config';
-import Material from '@primeng/themes/material';
 
 export const appConfig: ApplicationConfig = {
-  providers: [
-    provideAnimationsAsync(),
-    providePrimeNG({
-      theme: {
-          preset: Material
-      }
-    }),
-    provideZoneChangeDetection({ eventCoalescing: true }), 
-    provideRouter(routes)
-  ]
+  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes)]
 };
