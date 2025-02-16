f8b9811f6508414db9124521dcaa13dba8e9fd36
martrzeciak <mariusz.trzeciak09@gmail.com>
2025-02-16 18:15:12 +0100

    Add movie card component

--- client/movie-database/src/app/app.component.ts
+++ client/movie-database/src/app/app.component.ts
@@ -2,10 +2,11 @@ import { TuiRoot } from "@taiga-ui/core";
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from "./layouts/header/header.component";
+import { MovieCardComponent } from "./features/movies/movie-card/movie-card.component";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, TuiRoot, HeaderComponent],
+  imports: [RouterOutlet, TuiRoot, HeaderComponent, MovieCardComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
