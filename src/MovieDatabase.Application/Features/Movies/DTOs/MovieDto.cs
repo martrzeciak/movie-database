b709e5fc29fab0c597aadc2941c24ac983b660fc
martrzeciak <mariusz.trzeciak09@gmail.com>
2025-02-17 21:12:09 +0100

    Update MovieDto

--- src/MovieDatabase.Application/Features/Movies/DTOs/MovieDto.cs
+++ src/MovieDatabase.Application/Features/Movies/DTOs/MovieDto.cs
@@ -3,11 +3,11 @@
 public class MovieDto
 {
     public Guid Id { get; set; }
-    public string Title { get; set; } = default!;
-    public string Director { get; set; } = default!;
+    public string Title { get; set; } = string.Empty;
+    public string Director { get; set; } = string.Empty;
     public DateTime ReleaseDate { get; set; }
     public int DurationInMinutes { get; set; }
-    public string ContentRating { get; set; } = default!;
-    public string Description { get; set; } = default!;
+    public string ContentRating { get; set; } = string.Empty;
+    public string Description { get; set; } = string.Empty;
     public List<string> Genres { get; set; } = [];
 }
