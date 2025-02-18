e806d8b27223e9f4dce59798efc238cbb0063a95
martrzeciak <mariusz.trzeciak09@gmail.com>
2025-02-18 17:50:32 +0100

    Remove unnecessary ToString call from movie mapping configuration

--- src/MovieDatabase.Application/MappingConfigurations/MovieMappingConfiguration.cs
+++ src/MovieDatabase.Application/MappingConfigurations/MovieMappingConfiguration.cs
@@ -9,7 +9,7 @@ public class MovieMappingConfiguration : IRegister
     public void Register(TypeAdapterConfig config)
     {
         config.NewConfig<Movie, MovieDto>()
-            .Map(dest => dest.ContentRating, src => src.ContentRating.ToString())
+            .Map(dest => dest.ContentRating, src => src.ContentRating)
             .Map(dest => dest.Genres, src => src.Genres.Select(g => g.Name));
     }
 }
