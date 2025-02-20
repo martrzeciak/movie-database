c7babe87f48b38a667c7fee9430f4032e21ff423
martrzeciak <mariusz.trzeciak09@gmail.com>
2025-02-20 13:06:20 +0100

    Set validator initial value to null

--- src/MovieDatabase.Application/Behaviors/ValidationBehavior.cs
+++ src/MovieDatabase.Application/Behaviors/ValidationBehavior.cs
@@ -4,37 +4,27 @@ using MediatR;
 namespace MovieDatabase.Application.Behaviors;
 
 public class ValidationBehavior<TRequest, TResponse>
-    (IValidator<TRequest> validator)
+    (IValidator<TRequest>? validator = null)
     : IPipelineBehavior<TRequest, TResponse>
     where TRequest : IRequest<TResponse>
-    where TResponse : notnull
+    // where TRequest : notnull
 {
     public async Task<TResponse> Handle(TRequest request,
         RequestHandlerDelegate<TResponse> next,
         CancellationToken cancellationToken)
     {
+        // If the request does not implement IValidatable, then skip validation
         if (validator == null) return await next();
 
+        // Validate the request
         var context = new ValidationContext<TRequest>(request);
 
         var validationResult = await validator.ValidateAsync(request, cancellationToken);
 
+        // If the request is invalid, throw a ValidationException
         if (!validationResult.IsValid)
-        {
             throw new ValidationException(validationResult.Errors);
-        }
+
         return await next();
-    }
-}
-
-public class ValidationError
-{
-    public string PropertyName { get; set; }
-    public string ErrorMessage { get; set; }
-
-    public ValidationError(string propertyName, string errorMessage)
-    {
-        PropertyName = propertyName;
-        ErrorMessage = errorMessage;
     }
 }
