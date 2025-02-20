c49244d07401662951ec1429d63ea2497de518b4
martrzeciak <mariusz.trzeciak09@gmail.com>
2025-02-20 11:36:37 +0100

    Add create movie command validator

---
+++ src/MovieDatabase.Application/Features/Movies/Commands/CreateMovie/CreateMovieCommandValidator.cs
@@ -0,0 +1,12 @@
+﻿using MovieDatabase.Application.Features.Movies.DTOs;
+using MovieDatabase.Application.Features.Movies.Shared;
+
+namespace MovieDatabase.Application.Features.Movies.Commands.CreateMovie;
+
+public class CreateMovieCommandValidator : BaseMovieValidator<CreateMovieCommand, CreateMovieDto>
+{
+    public CreateMovieCommandValidator() : base(x => x.CreateMovieDto)
+    {
+        
+    }
+}
