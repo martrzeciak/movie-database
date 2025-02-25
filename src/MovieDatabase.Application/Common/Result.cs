cfe69c1f69c348f42c7dad61238c757d913947a0
martrzeciak <mariusz.trzeciak09@gmail.com>
2025-02-25 06:46:52 +0100

    Refactor Result class

--- src/MovieDatabase.Application/Common/Result.cs
+++ src/MovieDatabase.Application/Common/Result.cs
@@ -1,11 +1,15 @@
 ﻿namespace MovieDatabase.Application.Common;
 
-public class Result<T>
+public abstract class Result
 {
     public bool IsSuccess { get; set; }
-    public T? Value { get; set; }
     public string? Error { get; set; }
     public int Code { get; set; }
+}
+
+public class Result<T> : Result
+{
+    public T? Value { get; set; }
 
     public static Result<T> Success(T value) => new() { IsSuccess = true, Value = value };
     public static Result<T> Failure(string error, int code) => new()
